294b62bebdd676a4f2afce2417619054f7c0ef15
Jim7 <Jim7rfp@gmail.com>
2025-07-03 22:53:24 +0300

    Прорабатываем селектор городов.

--- frontend/assets/src/js/main.js
+++ frontend/assets/src/js/main.js
@@ -11,16 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	if (citySelect) {
 		citySelect.addEventListener('change', function () {
-			console.log('~~ 1 ', citySelect.value);
 			const slug = this.value;
 
-			console.log('~~ 2 ', slug);
 			// Сохраняем в cookie на 30 дней
 			// document.cookie = `city=${encodeURIComponent(selectedCity)}; path=/; max-age=${30 * 24 * 60 * 60}`;
 
 			// ↙️  убираем encodeURIComponent
-			document.cookie = `city=${slug}; path=/; max-age=${30*24*60*60}; SameSite=Lax`;
-			console.log('~~ 2 ', slug);
+			// document.cookie = `city=${slug}; path=/; max-age=${30*24*60*60}; SameSite=Lax`;
+			document.cookie = `city=${slug}; path=/; domain=${location.hostname}; max-age=${30*24*60*60}; SameSite=Lax`;
+
 
 			// Обновляем страницу
 			// location.reload();
