97c44a6a818eba2dfdd54e95870c1bfa3affa8cd
Jim7 <Jim7rfp@gmail.com>
2025-07-06 21:55:56 +0300

    Реорганизуем вывод большого числа услуг

--- frontend/assets/src/js/main.js
+++ frontend/assets/src/js/main.js
@@ -19,15 +19,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-	const btn = document.getElementById('toggleServicesBtn');
-	if (!btn) return;
-
-	btn.addEventListener('click', () => {
-		// ждём окончания bootstrap‑анимации collapse (350 мс по‑умолчанию)
-		setTimeout(() => {
-			const expanded = btn.getAttribute('aria-expanded') === 'true';
-			btn.textContent = expanded ? 'Скрыть услуги' : 'Показать все услуги';
-		}, 350);
-	});
-});
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const btn = document.getElementById('toggleServicesBtn');
+// 	if (!btn) return;
+//
+// 	btn.addEventListener('click', () => {
+// 		// ждём окончания bootstrap‑анимации collapse (350 мс по‑умолчанию)
+// 		setTimeout(() => {
+// 			const expanded = btn.getAttribute('aria-expanded') === 'true';
+// 			btn.textContent = expanded ? 'Скрыть услуги' : 'Показать все услуги';
+// 		}, 350);
+// 	});
+// });
