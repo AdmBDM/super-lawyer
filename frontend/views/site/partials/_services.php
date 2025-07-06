6d300d44cf2afc1f0ddfdd6272c1aa991da48981
Jim7 <Jim7rfp@gmail.com>
2025-07-06 19:00:02 +0300

    Причёсываем карточки услуг на главной

--- frontend/views/site/partials/_services.php
+++ frontend/views/site/partials/_services.php
@@ -1,30 +1,52 @@
 <?php
-
-use common\models\City;
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\StringHelper;
+use common\models\City;
 
-/** @var City $currentCity */
-/** @var array  $services */
-/** @var string $slug */
+/**
+ * @var City  $currentCity
+ * @var string $slug        // slug текущего города
+ * @var array $services     // массив услуг (asArray)
+ */
 
+$cityDative = Html::encode($currentCity->dative ?: $currentCity->name);
 ?>
-<section id="services" class="services-section py-5">
-	<div class="container">
-		<h2 class="mb-4 text-center">
-			Наши юридические услуги в <?= $currentCity->dative ?>
-		</h2>
 
-		<div class="row gy-4">
-            <?php foreach ($services as $sSlug => [$title, $desc]): ?>
+<section class="py-5 bg-light" id="services">
+	<div class="container">
+		<h2 class="mb-4 text-center">Юридические услуги в <?= $cityDative ?></h2>
+
+		<div class="row g-4">
+            <?php foreach ($services as $srv): ?>
 				<div class="col-12 col-md-6 col-lg-4">
-					<div class="card h-100 shadow-sm">
+					<div class="card h-100 shadow-sm border-0">
 						<div class="card-body d-flex flex-column">
-							<h5 class="card-title"><?= Html::encode($title) ?></h5>
-							<p class="card-text flex-grow-1"><?= Html::encode($desc) ?></p>
-							<a href="/<?= Html::encode($slug) ?>/<?= Html::encode($sSlug) ?>"
-							   class="btn btn-outline-primary mt-2 w-100">
-								Подробнее
-							</a>
+                            <?php if (!empty($srv['icon'])): ?>
+								<div class="mb-3 text-primary fs-1">
+									<i class="<?= Html::encode($srv['icon']) ?>"></i>
+								</div>
+                            <?php endif; ?>
+
+							<h5 class="card-title"><?= Html::encode($srv['title']) ?></h5>
+
+                            <?php if (!empty($srv['lead'])): ?>
+								<p class="card-text text-muted">
+                                    <?= Html::encode(StringHelper::truncate($srv['lead'], 100)) ?>
+								</p>
+                            <?php endif; ?>
+
+                            <?php if (!empty($srv['price_from'])): ?>
+								<p class="text-success fw-semibold mb-2">
+									от <?= Yii::$app->formatter->asCurrency($srv['price_from'], 'RUB') ?>
+								</p>
+                            <?php endif; ?>
+
+							<div class="mt-auto">
+								<a href="<?= Url::to("/{$slug}/{$srv['slug']}") ?>" class="btn btn-outline-primary w-100">
+									Подробнее
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
